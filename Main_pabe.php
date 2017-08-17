b6b67e1c4c64233d79ebc17f72e18875553bca4a
Max <satalk77@gmail.com>
2017-08-17 15:13:31 +0300

    first commit

---
+++ Main_pabe.php
@@ -0,0 +1,16 @@
+
+<html>
+        <head>
+            <meta charset-"utf-8";>
+            <title>Visual site></title>
+            <link rel="stylesheet" type="text/css" href="mystyle.css">
+        </head>
+<?php
+
+include "Header.html";
+include "Main.html";
+include "sidebar.html";
+include "left_menu.html";
+include "footer.html";
+
+?>
